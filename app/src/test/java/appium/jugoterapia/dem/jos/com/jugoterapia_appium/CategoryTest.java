b63cbee54c29a46e074af1b93a2f1056d9dcff54
josdem <joseluis.delacruz@gmail.com>
2018-10-12 15:18:48 -0400

    Adding Appium client and change the webdriver

--- app/src/test/java/appium/jugoterapia/dem/jos/com/jugoterapia_appium/CategoryTest.java
+++ app/src/test/java/appium/jugoterapia/dem/jos/com/jugoterapia_appium/CategoryTest.java
@@ -1,21 +1,38 @@
 package appium.jugoterapia.dem.jos.com.jugoterapia_appium;
 
+import android.util.Log;
+import android.widget.ListView;
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.experimental.categories.Category;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.AppiumDriver;
 
 public class CategoryTest {
 
-    WebDriver driver;
+
+    AppiumDriver driver;
+
+
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -27,15 +44,16 @@ public class CategoryTest {
         capabilities.setCapability("appPackage", "com.jugoterapia.josdem");
         capabilities.setCapability("appActivity", "com.jugoterapia.josdem.activity.CategoryActivity");
 
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void shouldSelectSaludable() {
-        driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
-        driver.findElement(By.name("7")).click();
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+    public void shouldSelectSaludable() throws Exception {
+        System.out.println("Running shouldSelectSaludable at " + new Date());
+        WebElement textView = driver.findElement(By.id("categoryTextView"));
+        System.out.print(textView);
+        //assertTrue(textView instanceof  RemoteWebDriver);
     }
 
     @After
