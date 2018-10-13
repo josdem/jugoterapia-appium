65a5b9ba350d5ccd3b64198dbae3f38bb79e6886
josdem <joseluis.delacruz@gmail.com>
2018-10-13 13:29:16 -0400

    #1 Adding category step

---
+++ app/src/test/java/steps/CategoryStep.java
@@ -0,0 +1,18 @@
+package steps;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CategoryStep {
+
+  @When("I launch the application")
+  public void shouldOpenTheApplication() throws Exception {
+
+  }
+
+  @Then("I should be able to see all categories")
+  public void shouldDisplayCategories() throws Exception {
+
+  }
+
+}
