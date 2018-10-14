2f90daba2de82ee54b0a3655143b33e4baf46152
josdem <joseluis.delacruz@gmail.com>
2018-10-13 20:02:54 -0400

    #1 Adding logger and before and after methods

--- app/src/test/java/com/jos/dem/appium/step/CategoryStep.java
+++ app/src/test/java/com/jos/dem/appium/step/CategoryStep.java
@@ -2,8 +2,6 @@ package com.jos.dem.appium.step;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
@@ -13,9 +11,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 
 import io.appium.java_client.AppiumDriver;
 
@@ -28,21 +29,28 @@ public class CategoryStep {
   private DesiredCapabilities capabilities = new DesiredCapabilities();
   private CategoryService categoryService = new CategoryServiceImpl();
 
+  private Logger log = Logger.getLogger(CategoryStep.class.getName());
+
+  @Before
+  public void setup(){
+    categoryService.setCapabilities(capabilities);
+  }
+
   @When("I launch the application")
   public void shouldOpenTheApplication() throws Exception {
-    categoryService.setCapabilities(capabilities);
+    log.info("Running: I launch the application at " + new Date());
     driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Then("I should be able to see all categories")
   public void shouldDisplayCategories() throws Exception {
-    System.out.println("Running shouldSelectSaludable at " + new Date());
+    log.info("Running: I should be able to see all categories at " + new Date());
     WebElement textView = driver.findElement(By.id("categoryTextView"));
     assertEquals("Curativos", textView.getText());
-    end();
   }
 
+  @After
   public void end(){
     driver.quit();
   }
