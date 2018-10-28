0a7e77f58a5cab2a5240df3c5c472e3ee91d3ab1
josdem <joseluis.delacruz@gmail.com>
2018-10-27 21:53:58 -0400

    #5 Moving the Cucumber Test to the right path

--- app/src/test/java/CucumberTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-
-import cucumber.api.junit.Cucumber;
-import cucumber.api.CucumberOptions;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Category.feature")
-public class CucumberTest {
-}
