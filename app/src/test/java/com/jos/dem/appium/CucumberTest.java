a037a7d5a23705783481adeb152834a236bd461f
josdem <joseluis.delacruz@gmail.com>
2018-10-27 21:54:21 -0400

    #5 Moving the Cucumber Test to the right path

---
+++ app/src/test/java/com/jos/dem/appium/CucumberTest.java
@@ -0,0 +1,11 @@
+package com.jos.dem.appium;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberTest {
+}
