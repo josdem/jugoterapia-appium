bdc68c88593c4c2f03069964b9ec11b3d7fc2d1e
josdem <joseluis.delacruz@gmail.com>
2018-11-03 19:58:30 -0400

    #9 Getting device name from properties

--- app/src/main/java/com/jos/dem/appium/service/impl/CategoryServiceImpl.java
+++ app/src/main/java/com/jos/dem/appium/service/impl/CategoryServiceImpl.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.jos.dem.appium.service.CategoryService;
+import com.jos.dem.appium.util.ConfigurationReader;
 
 public class CategoryServiceImpl implements CategoryService {
 
   public void setCapabilities(DesiredCapabilities capabilities){
-    capabilities.setCapability("deviceName", "Pixel 2");
+    capabilities.setCapability("deviceName", ConfigurationReader.getProperty("device.name"));
     capabilities.setCapability(CapabilityType.VERSION, "9");
     capabilities.setCapability("platformName", "Android");
     capabilities.setCapability("appPackage", "com.jugoterapia.josdem");
--- app/src/main/java/com/jos/dem/appium/util/ConfigurationReader.java
+++ app/src/main/java/com/jos/dem/appium/util/ConfigurationReader.java
@@ -1,6 +1,7 @@
 package com.jos.dem.appium.util;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
@@ -10,9 +11,17 @@ public class ConfigurationReader {
   private static Configurations configurations = new Configurations();
   private static File file = new File("src/main/res/configuration.properties");
 
-  public static String getProperty(String key) throws ConfigurationException {
-    Configuration configuration = configurations.properties(file);
-    return configuration.getString(key);
+  private static Logger log = Logger.getLogger(ConfigurationReader.class.getName());
+
+  public static String getProperty(String key){
+    String value = "";
+    try{
+      Configuration configuration = configurations.properties(file);
+      value = configuration.getString(key);
+    }catch(ConfigurationException cex){
+      log.warning(cex.getMessage());
+    }
+    return value;
   }
 
 }
