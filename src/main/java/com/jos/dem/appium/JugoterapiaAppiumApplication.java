55e4fbd8c2495c7a2c407178577e09e0018eaa21
josdem <joseluis.delacruz@gmail.com>
2018-11-13 15:48:55 -0500

    #13 Adding JugoterapiaAppium main class

---
+++ src/main/java/com/jos/dem/appium/JugoterapiaAppiumApplication.java
@@ -0,0 +1,14 @@
+package com.jos.dem.appium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiugoterapiaAppiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugoterapiaAppiumApplication.class, args);
+  }
+
+}
+
