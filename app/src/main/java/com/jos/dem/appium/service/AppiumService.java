2a2b6bb0f22a8e37134b4d255e30c5466707f786
josdem <joseluis.delacruz@gmail.com>
2019-03-05 18:00:30 -0500

    #17 Adding Appium Service

---
+++ app/src/main/java/com/jos/dem/appium/service/AppiumService.java
@@ -0,0 +1,12 @@
+package com.jos.dem.appium.service;
+
+import java.io.IOException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public interface AppiumService {
+
+  void setCapabilities(DesiredCapabilities capabilities) throws IOException;
+
+}
+
+
